c8730d8635eb0c507cab5b85e19d7afc4bdc195b
Denys Mitiachkin <denismityachkin@gmail.com>
2024-05-08 20:10:59 +0300

    add binary tree

---
+++ src/binaryTree.ts
@@ -0,0 +1,56 @@
+
+
+/**
+ * BINARY TREE
+ * Task: sum all the values from every level of concern
+ */
+
+type TreeNode = {
+    value: number;
+    left?: TreeNode;
+    right?: TreeNode;
+};
+
+const binaryTree: TreeNode = {
+    value: 7,
+    left: {
+        value: 5,
+        left: {
+            value: 5,
+            left: {
+                value: 5,
+            },
+            right: {
+                value: 4,
+                right: {
+                    value: 4,
+                    right: {
+                        value: 4,
+                    },
+                },
+            },
+        },
+        right: {
+            value: 4,
+        },
+    },
+    right: {
+        value: 4,
+    },
+};
+
+const sumTree = (tree: TreeNode): number => {
+    let sum = 0;
+    if (tree.value) {
+        sum += tree.value;
+    }
+    if (tree.left) {
+        sum += sumTree(tree.left);
+    }
+    if (tree.right) {
+        sum += sumTree(tree.right);
+    }
+    return sum;
+};
+
+console.log(sumTree(binaryTree));
---
+++ src/miscellaneous.ts
@@ -0,0 +1,19 @@
+/**
+ * TASK 1: Check if property exists in the object
+ */
+
+const obj = { name: 'a', age: 1 };
+const exist_1 = Object.keys(obj).includes('age')
+const exist_2 = obj.hasOwnProperty('age')
+const exist_3 = 'age' in obj
+console.log(exist_1, exist_2, exist_3);
+
+
+/**
+ * TASK 2: 
+ */
+
+
+/**
+ * TASK 3: 
+ */
